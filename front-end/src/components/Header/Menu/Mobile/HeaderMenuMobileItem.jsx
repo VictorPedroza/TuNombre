8f73f2155712192b8cb6fe7ed05b24403dd15417
VictorPedroza <Victor242206@gmail.com>
2025-06-07 23:05:42 -0300

    fix: update styles for mobile menu items and list for consistency

--- front-end/src/components/Header/Menu/Mobile/HeaderMenuMobileItem.jsx
+++ front-end/src/components/Header/Menu/Mobile/HeaderMenuMobileItem.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom"
 export const MobileItem = ({ to, text, setIsOpen }) => {
     return (
         <li>
-            <NavLink to={to} className="block px-4 py-2 text-blue-500 hover:bg-green-200" onClick={() => setIsOpen(false)}>{text}</NavLink>
+            <NavLink to={to} className="block px-4 py-2 text-primary-500" onClick={() => setIsOpen(false)}>{text}</NavLink>
         </li>
 
     )
--- front-end/src/components/Header/Menu/Mobile/HeaderMenuMobileList.jsx
+++ front-end/src/components/Header/Menu/Mobile/HeaderMenuMobileList.jsx
@@ -2,7 +2,7 @@ export const MobileList = ({ isOpen, children }) => {
     return (
         <ul
             className={`
-                        absolute right-0 mt-2 w-40 bg-green-100 shadow-md rounded-md flex flex-col py-2 z-50 md:hidden
+                        absolute right-0 mt-2 w-64 bg-white shadow-md border border-black/20 rounded-md flex flex-col py-2 z-50 md:hidden
                         transition-all duration-300 origin-top
                         ${isOpen ? 'opacity-100 scale-100 translate-y-0' : 'opacity-0 scale-95 -translate-y-2 pointer-events-none'}
                     `}
