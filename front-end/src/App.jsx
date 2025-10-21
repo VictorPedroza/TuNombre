99e88e00e7f7a602ed15d35c43b40edf11fc920a
VictorPedroza <Victor242206@gmail.com>
2025-10-21 20:45:06 -0300

    refactor: clean up App component by removing notification logic and update MainProvider import path

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,15 +1,8 @@
-import { NotificationContext } from "@/contexts";
-import { useContext } from "react";
-
 function App() {
-
-  const { addNotification } = useContext(NotificationContext);
-
   return (
     <>
-      <div className='flex flex-col gap-2 h-screen w-screen p-8 bg-white' >
-        <h1>Teste de Notificação</h1>
-        <button onClick={() => addNotification({ title: 'Teste', subtitle: 'Teste' })} >Adicionar</button>
+      <div className='flex flex-col gap-2 h-screen w-screen bg-white' >
+        <h1>TuNombre</h1>
       </div>
     </>
   )
