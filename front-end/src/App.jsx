b4658aba528c3734e1e8f7927d9d046de75e002e
VictorPedroza <Victor242206@gmail.com>
2025-10-22 20:28:41 -0300

    feat: update App component layout to use Sidebar and enhance styling

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,8 +1,17 @@
+import { Sidebar } from "@/components";
+
 function App() {
   return (
     <>
-      <div className='flex flex-col gap-2 h-screen w-screen bg-white' >
-        <h1>TuNombre</h1>
+      <div className='flex h-screen w-screen bg-gradient-to-tr from-slate-500 to-slate-800 lg:px-12 lg:py-8'>
+        <div className="bg-gray-50 w-full h-full lg:rounded-lg shadow-lg flex overflow-hidden">
+          <Sidebar.Root>
+            <Sidebar.Head />
+          </Sidebar.Root>
+          <main className="w-full h-full px-4 py-3">
+            Content
+          </main>
+        </div>
       </div>
     </>
   )
