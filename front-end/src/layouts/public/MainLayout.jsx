bee13aa49e7b80d5b5edfce0f7bff6fe2d2e2224
VictorPedroza <victor242206@gmail.com>
2025-12-04 19:21:35 -0300

    feat(front-end): update MainLayout and AppRoutes with improved documentation and routing structure

--- front-end/src/layouts/public/MainLayout.jsx
+++ front-end/src/layouts/public/MainLayout.jsx
@@ -1,5 +1,12 @@
 import { Outlet } from 'react-router-dom';
 
+/**
+ * Layout Principal da Aplicação
+ * 
+ * @author Victor Pedroza <victor242206@gmail.com>
+ * @since 01/12/2025
+ * @version 1.0.0
+**/
 export const MainLayout = () => {
     return(
         <main>
--- front-end/src/routes/AppRoutes.jsx
+++ front-end/src/routes/AppRoutes.jsx
@@ -1,13 +1,23 @@
 import { Routes, Route } from 'react-router-dom';
 
-import { MainLayout } from '@/layouts';
+import { AuthLayout, MainLayout } from '@/layouts';
 
+/**
+ * Controlador das Rotas da Aplicação 
+ * 
+ * @author Victor Pedroza <victor242206@gmail.com>
+ * @since 01/12/2025
+ * @version 1.0.1
+**/
 export const AppRoutes = () => {
     return(
         <>
             <Routes>
                 <Route path='/' element={<MainLayout />}>
                     <Route index element={<h1>Teste</h1>} />
+                </Route>
+                <Route path="auth" element={<AuthLayout />}>
+                    <Route path="login" element={<h1>Login</h1>}/>
                 </Route>
             </Routes>
         </>
