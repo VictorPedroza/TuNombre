76a0ed9ed13542b02204ec4bd6d0ddf5e243edd5
VictorPedroza <victor242206@gmail.com>
2025-12-06 16:45:10 -0300

    feat(front-end): refactor MainLayout to use Sidebar component for navigation

--- front-end/src/layouts/public/MainLayout.jsx
+++ front-end/src/layouts/public/MainLayout.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { IoMenu, IoClose } from "react-icons/io5";
-import { GoHome } from "react-icons/go";
+import { Sidebar } from '@/components';
+
+import { routes } from '@/routes';
+
 /**
  * Layout Principal da Aplicação
  * 
@@ -18,30 +21,16 @@ export const MainLayout = () => {
 
     return (
         <main className='w-full h-full bg-gray-50 lg:rounded-2xl overflow-hidden shadow-xl flex'>
-            <aside className={`lg:min-w-64 min-w-48 bg-white shadow-md border-r border-gray-50 z-10 sm:block ${isOpen ? 'block absolute h-full' : 'hidden'} lg:block`}>
-                <div className='w-full h-12 mt-2 flex items-center justify-center'>
-                    <Link
-                        to='/'
-                        className='text-3xl font-bold text-green-600 flex'
-                    >
-                        <p>Tu</p>
-                        <span className='text-red-600'>Nombre</span>
-                    </Link>
-                </div>
-                <nav className='w-full mt-6'>
-                    <ul className='w-full h-full flex flex-col px-8'>
-                        <Link to='/' className='w-full text-slate-700 bg-gray-500 backdrop-filter backdrop-blur-md bg-opacity-0 px-4 py-2 rounded-2xl flex items-center gap-2 '>
-                            <GoHome className='text-2xl' />
-                            <p className='text-xl font-semibold'>Início</p>
-                        </Link>
-                    </ul>
-                </nav>
-                <div>
-                    
-                </div>
-            </aside>
-            <main className='w-full h-full flex flex-col'>
-                <header className='w-full h-12 bg-white/90 border-b border-gray-100 flex items-center px-4 justify-between'>
+            <Sidebar.Root isOpen={isOpen}>
+                <Sidebar.Head />
+                <Sidebar.Navigation>
+                    {routes.map((page, index) => (
+                        <Sidebar.NavItem key={index} to={page.to} icon={page.icon} label={page.label} />
+                    ))}
+                </Sidebar.Navigation>
+            </Sidebar.Root>
+            <div className='w-full h-full flex flex-col'>
+                <header className='w-full h-14 bg-white/90 border-b border-gray-100 flex items-center px-4 justify-between'>
                     <h1 className='text-xl font-semibold text-green-700'>Header</h1>
                     <button className='block sm:hidden text-4xl text-green-600' onClick={() => toggleSidebar()}>
                         {isOpen ? (<IoClose />) : (<IoMenu />)}
@@ -50,7 +39,7 @@ export const MainLayout = () => {
                 <div className='w-full h-full px-4 py-3'>
                     <Outlet />
                 </div>
-            </main>
+            </div>
         </main>
     )
 }
