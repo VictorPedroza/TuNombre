6de93a3e7cbc9656b0cb4fdbed4c25fe1a9e1d0d
VictorPedroza <victor242206@gmail.com>
2025-12-06 17:33:42 -0300

    fix(App): correct padding values in App component

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,7 +2,7 @@ import { AppRoutes } from "@/routes/AppRoutes";
 
 function App() {
   return(
-    <div className="w-screen h-screen bg-linear-to-r from-red-500 from-10% via-red-600 via-40% to-red-700 to-90% lg:px-18 lg:py-10">
+    <div className="w-screen h-screen bg-linear-to-r from-red-500 from-10% via-red-600 via-40% to-red-700 to-90% lg:px-14 lg:py-10">
       <AppRoutes />
     </div>
   )
