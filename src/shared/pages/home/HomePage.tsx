f3e16e7c4125e239be67216eafbf501f6feb6cc0
VictorPedroza <victor.pedroza@protonmail.com>
2026-04-08 19:29:06 -0300

    ✨ feat:(pages): Add Hero Component on Home Page

--- src/shared/pages/home/HomePage.tsx
+++ src/shared/pages/home/HomePage.tsx
@@ -1,9 +1,16 @@
+import { Hero } from "@shared/pages/home/components";
+
 const HomePage = () => {
-    return(
-        <div>
-            <h1>Home Page</h1>
-        </div>
-    )
-}
+  return (
+    <div className="flex flex-col items-center justify-center px-4 py-12">
+      <div className="text-center">
+        <Hero />
+      </div>
+      <div className="mt-24">
+        <h1></h1>
+      </div>
+    </div>
+  );
+};
 
 export default HomePage;
