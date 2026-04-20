2f90815920d1ab991a05951fe55ba1c689c67923
VictorPedroza <victor.pedroza@protonmail.com>
2026-04-20 20:55:30 -0300

    ✨ feat:(home-page): Add new components on home page

--- src/shared/pages/home/HomePage.tsx
+++ src/shared/pages/home/HomePage.tsx
@@ -1,14 +1,17 @@
 import { Hero } from "@shared/pages/home/components";
 import { Timer } from "./components/timer/Timer";
-
+import { DailyMessage } from "./components/message/Message";
+import { LoveLetter } from "./components/letter/Letter";
 const HomePage = () => {
   return (
-    <div className="flex flex-col items-center justify-center px-4 py-12">
+    <div className="w-full h-min-screen flex flex-col items-center justify-center px-4 py-12">
       <div className="text-center">
         <Hero />
       </div>
-      <div>
+      <div className="w-full max-w-lg">
         <Timer />
+        <DailyMessage />
+        <LoveLetter />
       </div>
     </div>
   );
