3b23ed3818f9ee3fcad26729c3ad515eaa1e7545
VictorPedroza <victor.pedroza@protonmail.com>
2026-07-03 10:07:54 -0300

    ✨ feat:(environment): add supabase url and key on enviroment

--- src/core/env/environment.ts
+++ src/core/env/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   appName: import.meta.env.VITE_APP_NAME,
-  enviroment: import.meta.env.VITE_ENVIRONMENT,
+  enviroment: import.meta.env.VITE_ENVIRONMENT === "production",
+  supabaseUrl: import.meta.env.VITE_SUPABASE_URL,
+  supabasePublishableKey: import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY,
 } as const;
