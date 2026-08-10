c0e92f3b9414cf90a2b55f8e9bee6b87f77b322c
VictorPedroza <victor.pedroza@protonmail.com>
2026-08-10 20:02:38 -0300

    ✨ feat:(admin): Adiciona a funcionalidade de abrir e fechar a barra lateral

--- src/core/routes/layouts/admin/AdminLayout.tsx
+++ src/core/routes/layouts/admin/AdminLayout.tsx
@@ -1,10 +1,17 @@
+import { Sidebar } from "@/shared/components";
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 
 export const AdminLayout = () => {
+  const [menuOpen, setMenuOpen] = useState(true);
+
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center px-6 bg-[#121212]">
-        Admin Layout
+    <div className="min-h-screen flex bg-[#121212]">
+      <Sidebar mobileOpen={menuOpen} onClose={() => setMenuOpen(false)} />
+
+      <main className="flex-1 lg:ml-60 p-6">
         <Outlet />
+      </main>
     </div>
   );
 };
