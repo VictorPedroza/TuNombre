64101a0a79f2b060ed89a7038a4279983872d0fb
VictorPedroza <victor.pedroza@protonmail.com>
2026-08-12 07:19:33 -0300

    ✨ feat:(layout): Aplica Header do admin no layout de admin

--- src/core/routes/layouts/admin/AdminLayout.tsx
+++ src/core/routes/layouts/admin/AdminLayout.tsx
@@ -1,3 +1,4 @@
+import { AdminHeader } from "@/modules/admin";
 import { Sidebar } from "@/shared/components";
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
@@ -9,8 +10,11 @@ export const AdminLayout = () => {
     <div className="min-h-screen flex bg-[#121212]">
       <Sidebar mobileOpen={menuOpen} onClose={() => setMenuOpen(false)} />
 
-      <main className="flex-1 lg:ml-60 p-6">
-        <Outlet />
+      <main className="flex-1 lg:ml-60">
+        <AdminHeader onOpenMenu={() => setMenuOpen(true)} />
+        <div className="p-6">
+          <Outlet />
+        </div>
       </main>
     </div>
   );
