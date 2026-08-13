5802590f4541b96c4de3cc60093c8fc829d80fd5
VictorPedroza <victor.pedroza@protonmail.com>
2026-08-13 17:06:51 -0300

    ✨ feat:(history): Aplica a página de história nas rotas

--- src/core/routes/routes.ts
+++ src/core/routes/routes.ts
@@ -1,21 +1,22 @@
-import { HomeIcon, LayoutDashboard } from "lucide-react";
+import { HomeIcon, LayoutDashboard, TimerIcon } from "lucide-react";
 
 import type { Layout } from "@/shared/types/Routes";
 
-import { AuthLayout, MainLayout } from "@/core/routes/layouts";
+import { AdminLayout, AuthLayout, MainLayout } from "@/core/routes/layouts";
 
 import { HomePage } from "@/modules/home";
 import { LoginPage } from "@/modules/auth";
-import { AdminLayout } from "./layouts/admin/AdminLayout";
-import { AdminPage } from "@/modules/admin/pages/AdminPage";
+import { HistoryPage } from "@/modules/history";
+import { AdminPage } from "@/modules/admin";
 
 export const routes: Layout[] = [
   {
-    path: "/",
+    path: "",
     private: false,
     component: MainLayout,
     children: [
       { path: "", label: "Inicio", component: HomePage, icon: HomeIcon },
+      { path: "historia", label: "História", component: HistoryPage, icon: TimerIcon },
     ],
   },
   {
