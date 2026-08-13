a25f392be7961313853f55959cf60285d9eb93d4
VictorPedroza <victor.pedroza@protonmail.com>
2026-08-13 17:08:56 -0300

    📚 docs:(history): Adiciona o JSDOC na página de história

--- src/modules/history/pages/HistoryPage.tsx
+++ src/modules/history/pages/HistoryPage.tsx
@@ -1,3 +1,11 @@
+/**
+ *  Interface da Página da História
+ * 
+ * @author Victor Pedroza <victor.pedroza@protonmail.com>
+ * @since 2026-08-13
+ * @version 1.0.0
+ * 
+ **/
 export const HistoryPage = () => {
     return (
         <div className="flex flex-col items-center justify-center">
