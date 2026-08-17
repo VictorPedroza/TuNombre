fb90687d9b888ec110948664320e23189fc3fd3d
VictorPedroza <victor.pedroza@protonmail.com>
2026-08-17 07:01:51 -0300

    ✨ feat:(overview): Atualiza AdminPage para OverviewPage

--- src/modules/admin/index.ts
+++ src/modules/admin/index.ts
@@ -1,2 +1,1 @@
-export * from "./components"
-export * from "./pages/AdminPage"
+export * from "./pages/OverviewPage"
--- src/modules/admin/pages/OverviewPage.tsx
+++ src/modules/admin/pages/OverviewPage.tsx
@@ -1,4 +1,4 @@
-export const AdminPage = () => {
+export const OverviewPage = () => {
     return (
         <h1 className="text-2xl font-bold text-white">Admin Page</h1>
     )
