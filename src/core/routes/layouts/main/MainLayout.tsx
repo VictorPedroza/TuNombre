8a73d9521b273db53cb9be741d4c39ed0e276b4a
VictorPedroza <victor.pedroza@protonmail.com>
2026-08-18 22:39:43 -0300

    📚 docs:: Adiciona o JSDOC aos Layouts

--- src/core/routes/layouts/main/MainLayout.tsx
+++ src/core/routes/layouts/main/MainLayout.tsx
@@ -1,6 +1,14 @@
 import { Footer, Header } from "@/shared/components";
 import { Outlet } from "react-router-dom";
 
+/**
+ * Layout Principal da Aplicação 
+ * 
+ * @author Victor Pedroza <victor.pedroza@protonmail.com>
+ * @since 2026-07-03
+ * @version 1.0.0
+ * 
+ **/
 export const MainLayout = () => {
   return (
     <>
