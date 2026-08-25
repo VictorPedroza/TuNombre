8698363b094e3140b166cc7db46880859d816820
VictorPedroza <victor.pedroza@protonmail.com>
2026-08-25 18:14:54 -0300

    ✨ feat:(components): Adiciona o título da página no componente de cabeçalho

--- src/shared/components/layouts/header/components/AdminHeader.tsx
+++ src/shared/components/layouts/header/components/AdminHeader.tsx
@@ -1,10 +1,23 @@
+import { routes } from "@/core/routes/routes";
 import { Menu } from "lucide-react"
+import { useLocation } from "react-router-dom";
 
 interface AdminHeaderProps {
     onOpenMenu: () => void;
 }
 
 export const AdminHeader = ({ onOpenMenu }: AdminHeaderProps) => {
+    const location = useLocation();
+
+    const layout = routes.find((route) => route.path === "/admin");
+
+    const currentRoute = layout?.children.find((child) => {
+        const fullPath = child.path === "" ? "/admin" : `/admin/${child.path}`;
+        return fullPath === location.pathname;
+    })
+
+    const pageTitle = currentRoute?.label || "Overview";
+
     return (
         <header className="h-16 border-b border-foreground-muted/10 flex items-center gap-4 px-6 flex-shrink-0 bg-[#121212]">
             <button
@@ -13,7 +26,7 @@ export const AdminHeader = ({ onOpenMenu }: AdminHeaderProps) => {
             >
                 <Menu size={20} />
             </button>
-            <h1 className="text-base font-medium text-background">Overview</h1>
+            <h1 className="text-base font-medium text-background">{pageTitle}</h1>
         </header>
 
     )
