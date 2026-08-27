6a12b1091f68f1fed3fafca761d234770482e3c4
VictorPedroza <victor.pedroza@protonmail.com>
2026-08-27 19:59:19 -0300

    ✨ feat:(timeline): Exporta componentes da linha do tempo

--- src/modules/timeline/components/index.ts
+++ src/modules/timeline/components/index.ts
@@ -1,8 +1,11 @@
-export * from "./card/SumaryCard";
-export * from "./card/UnitCard";
+export * from "./summary/SummaryCard";
+export * from "./summary/UnitCard";
+export * from "./summary/Summary";
 
-export * from "./timeline/TimelineHead"
-export * from "./timeline/TimelineDesktop"
-export * from "./timeline/TimelineMobile"
 export * from "./timeline/TimelineModal"
 export * from "./timeline/TimelineTable"
+export * from "./timeline/TimelineCard"
+
+export * from "./timeline/TimelineDesktop"
+export * from "./timeline/TimelineMobile"
+export * from "./timeline/Timeline"
