5441d38985b7652c46e6bfef0bfc627ac0338b34
VictorPedroza <victor.pedroza@protonmail.com>
2026-08-27 18:10:06 -0300

    ✨ feat:(components): Adiciona componente de caixa de texto

--- src/modules/auth/pages/login/LoginPage.tsx
+++ src/modules/auth/pages/login/LoginPage.tsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 
 import { Eye, EyeOff } from "lucide-react";
 
-import { Input } from "../../components";
 import { useLoginForm } from "../../hooks/useLoginForm";
+import { Input } from "@/shared/components";
 
 /**
  * Página de Login da Aplicaçãp
--- src/shared/components/common/index.ts
+++ src/shared/components/common/index.ts
@@ -1,2 +1,3 @@
 export * from "./button/Button"
 export * from "./check-box/CheckBox"
+export * from "./input/Input"
